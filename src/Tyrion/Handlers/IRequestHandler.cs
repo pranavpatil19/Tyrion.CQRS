5d6c59008c8b92dde4035e337a6540bc418180e6
Lucas Luiz da Silva Santos <lucas.santos@confitec.com.br>
2020-03-13 10:09:40 -0300

    .

---
+++ src/Tyrion/Handlers/IRequestHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Tyrion
+{
+    public interface IRequestHandler<in TRequest, TResult> where TRequest : IRequest
+    {
+        Task<IResult<TResult>> Execute(TRequest request);
+    }
+
+    public interface IRequestHandler<in TRequest> where TRequest : IRequest
+    {
+        Task<IResult> Execute(TRequest request);
+    }
+}
---
+++ src/Tyrion/Results/Result.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+
+namespace Tyrion
+{
+    public class Result : IResult
+    {
+        protected Result() => Successed = true;
+
+        protected Result(string message, bool successed)
+        {
+            Successed = successed;
+            Message = message;
+        }
+
+        public bool Successed { get; protected set; }
+
+        public bool Failed => !Successed;
+
+        public string Message { get; protected set; }
+
+        public static IResult Success() => new Result();
+
+        public static IResult Success(string message) => new Result(message, true);
+
+        public static Task<IResult> SuccessAsync() => Task.FromResult(Success());
+
+        public static Task<IResult> SuccessAsync(string message) => Task.FromResult(Success(message));
+
+        public static IResult Fail(string message) => new Result(message, false);
+
+        public static Task<IResult> FailAsync(string message) => Task.FromResult(Fail(message));
+    }
+
+    public sealed class Result<T> : Result, IResult<T>
+    {
+        private Result() => Successed = true;
+
+        private Result(T data)
+        {
+            Successed = true;
+            Data = data;
+        }
+
+        private Result(string message) => Message = message;
+
+        public T Data { get; }
+
+        public new static IResult<T> Success() => new Result<T>();
+
+        public static IResult<T> Success(T data) => new Result<T>(data);
+
+        public static Task<IResult<T>> SuccessAsync(T data) => Task.FromResult(Success(data));
+
+        public new static IResult<T> Fail(string message) => new Result<T>(message);
+
+        public new static Task<IResult<T>> FailAsync(string message) => Task.FromResult(Fail(message));
+    }
+}
